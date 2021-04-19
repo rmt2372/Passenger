d9ee4e917268fa55c90cd77da3e6e4c06caf1b2b
annguyen32 <annnguyen32@gmail.com>
2021-04-19 02:46:55 -0500

    review page/php

---
+++ review.php
@@ -0,0 +1,19 @@
+<?php
+  # get the incoming information
+  $name = $_POST["name"];
+  $review = $_POST["review"];
+
+  # Write thank you page
+  print <<<REGISTRATION_RESULT
+  <html>
+  <head>
+  <title> THANK YOU FOR YOUR REVIEW  </title>
+  </head>
+  <body>
+  <h1> Thank You for Reviewing </h1>
+  <h2> An e-mail confirmation will be sent to you. </h2>
+  </body>
+  </html>
+REGISTRATION_RESULT;
+
+?>
